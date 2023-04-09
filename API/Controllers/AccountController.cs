2daae8066be460cf126f8e7e99c6db66b0c395da
Jorge Espartero <jorge.espartero@getronics.com>
2023-04-09 12:41:10 +0200

    End of Section 4

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,80 @@
+using System.Security.Cryptography;
+using System.Text;
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace API.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        private readonly DataContext _context;
+        private readonly ITokenService _tokenService;
+        public AccountController(DataContext context, ITokenService tokenService)
+        {
+            _tokenService = tokenService;
+            _context = context;
+        }
+        // EndPoint para registrar un usuario
+        [HttpPost("register")] // POST: api/account/register?username=dave&password=pwd
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+        {
+            if (await UserExists(registerDto.UserName)) return BadRequest("Username is taken");
+
+            using var hmac = new HMACSHA512();
+
+            var user = new AppUser
+            {
+                UserName = registerDto.UserName.ToLower(),
+                PassWordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+                PasswordSalt = hmac.Key
+            };
+            // añadimos el usuario al context
+            _context.Users.Add(user);
+            // GUARDAMOS el usuario en BBDD
+            await _context.SaveChangesAsync();
+            // devolvemos el nuevo usuario añadido en BBDD
+            return new UserDto
+            {
+                Username = user.UserName,
+                Token = _tokenService.CreateToken(user)
+            };
+        }
+
+        [HttpPost("login")] // POST api/account/login
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.UserName);
+
+            if(user == null) return Unauthorized("User name not valid.");
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+            // convertimos el HASH con el SALT del usuario para poder ser tratado y verificar bit a bit si son iguales
+            var computeHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            // ahora comprobamos si el array de bits de la contraseña recibida es igual que la calculada de bbdd
+            for(int i = 0; i < computeHash.Length; i++)
+            {
+                if (computeHash[i] != user.PassWordHash[i]) return Unauthorized("Password not valid");
+            }
+            // si todo es correcto, devolvemos el user de BBDD
+            return new UserDto
+            {
+                Username = user.UserName,
+                Token = _tokenService.CreateToken(user)
+            };
+        }
+
+        // esta función mira si existe el usuario en bbdd con el mismo username indicado
+        private async Task<bool> UserExists(string username)
+        {
+            // recordar que await es para indicar algo a la BBDD
+            return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
+        }
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,16 +1,21 @@
+using System.Text;
 using API.Data;
+using API.Extensions;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt => 
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-builder.Services.AddCors();
+// a continuación la extensión de servicios creada \API\Extensions\ApplicationServiceExtensions.cs
+builder.Services.AddApplicationServices(builder.Configuration);
+// a continuación la extensión de servicios creada \API\Extensions\IdentityServiceExtensions.cs
+builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 
@@ -18,6 +23,8 @@ var app = builder.Build();
 // JorgeEsp -> siguiente línea es para indicar que se confía el acceso a localhost:4200 desde localhost:5001
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
+app.UseAuthentication(); // esto preguntará si hay un Token válido
+app.UseAuthorization();  // esto pregunta si el user está autorizado
 
 app.MapControllers();
 
